fd4c8ef5862e2e7a791a3cceaaa38aadad650e09
CristianoMends <mendescristiano012@gmail.com>
2023-03-19 14:31:10 -0300

    Layout principal

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+function gowhatsapp(){    
+        window.open("https://wa.me/qr/3X3KO4KZIJXKI1",
+                "", "width=400, height=400");
+    
+}
+function goemail(){    
+    window.open("mailto:mendescristiano012@gmail.com",
+            "", "width=400, height=400");
+
+}
+function golinkedin(){    
+    window.open("https://www.linkedin.com/in/cristiano-mendes-link/",
+            "", "width=400, height=400");
+
+}
+function gogithub(){    
+        window.open("https://github.com/CristianoMends",
+                "", "width=400, height=400");
+    
+}
